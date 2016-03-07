94167b522044a32deb5337fb47dc05b3c011dfe2
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-03-08 10:26:12 +1100

    Starting to build html and css

--- js/project.js
+++ js/project.js
@@ -5,6 +5,25 @@
 // Check out Tutorial and Documentation resources (jQuery tutorial) at home to better understand what you’ll be getting into.
 // Don’t be afraid to write code that you know you will have to remove later. Create temporary elements (buttons, links, etc) that trigger events if real data is not available. For example, if you’re trying to figure out how to change some text when the game is over but you haven’t solved the win/lose game logic, you can create a button to simulate that until then.
 //
-//functionality and logic /////   presentation ui 
+//functionality and logic /////   presentation ui
 // current move             //      display current move
 //clickhandlers             //      display move
+
+//virtual version which logs the front end choices
+
+$( document ).ready(function() {
+
+// Changes color once box has been clicked
+  $(".box").on("click", function () {
+  $(this).css('background-color', 'firebrick');
+  });
+// once a box has been clicked and changed colour
+// next clicked box changes to a different colour
+
+//a function, array or something logs these colour changes
+//and stores them
+
+//once three of these are logged in a 3 then game returns winner
+// if it is a tie without 3 in a row game calls tie
+
+});
--- readme.md
+++
@@ -1,2 +0,0 @@
-# Project0
-my first project
