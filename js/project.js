14a482b1a3506635d96cb52cdb6eefa5475711dd
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-03-08 16:15:48 +1100

    working out winner

--- js/project.js
+++ js/project.js
@@ -4,26 +4,84 @@
 // Commit early, commit often. Don’t be afraid to break something because you can always go back in time to a previous version.
 // Check out Tutorial and Documentation resources (jQuery tutorial) at home to better understand what you’ll be getting into.
 // Don’t be afraid to write code that you know you will have to remove later. Create temporary elements (buttons, links, etc) that trigger events if real data is not available. For example, if you’re trying to figure out how to change some text when the game is over but you haven’t solved the win/lose game logic, you can create a button to simulate that until then.
-//
-//functionality and logic /////   presentation ui
-// current move             //      display current move
-//clickhandlers             //      display move
 
-//virtual version which logs the front end choices
+$(document).ready(function() {
 
-$( document ).ready(function() {
 
-// Changes color once box has been clicked
-  $(".box").on("click", function () {
-  $(this).css('background-color', 'firebrick');
+  var counter = 0;
+  // Array is created to check against player classes to determine winner.
+  var player1Array = [];
+  var player2Array = [];
+  // if a box is clicked counter is up by one
+  // if another box is clicked counter goes down by one
+
+  // CHECKING HORIZONTALS
+  var checkWin = function(playerArray, winner) {
+
+    if (playerArray.includes("#1") && playerArray.includes("#2"), playerArray.includes ("#3")) {
+      alert(winner + " wins");
+    // } else
+    // if (playerArray.includes("#4" && "#5" && "#6")) {
+    //   alert(winner + " wins");
+    // } else
+    // if (playerArray.includes("#7" && "#8" && "#9")) {
+    //   alert(winner + " wins");
+    // } else
+    //
+    // // CHECKING VERTICALS
+    // if (playerArray.includes("#1" && "#4" && "#7")) {
+    //   alert(winner + " wins");
+    // } else
+    // if (playerArray.includes("#2" && "#5" && "#8")) {
+    //   alert(winner + " wins");
+    // } else
+    // if (playerArray.includes("#3" && "#6" && "#9")) {
+    //   alert(winner + " wins");
+    // } else
+    //
+    // // CHECKING DIAGONALS
+    //
+    // if (playerArray.includes("#7" && "#5" && "#3")) {
+    //   alert(winner + " wins");
+    // } else
+    // if (playerArray.includes("#9" && "#5" && "#1")) {
+    //   alert(winner + " wins");
+    }
+  };
+
+
+  // Counter alternates between player one and player 2
+  // Changes color once box has been clicked
+  $(".box").on("click", function() {
+    if (counter < 1) {
+      counter++;
+      $(this).css('background-color', 'red');
+      // Adds the player class
+      $(this).addClass('player1');
+      // Turns off click function after initial click
+      $(this).off("click");
+      // adds the current boxes id to relevent array
+      player1Array.push('#' + this.id);
+      // runs function to check win
+      checkWin(player1Array, "player 1");
+      console.log(player1Array);
+    } else if (counter === 1) {
+      counter -= 1;
+      $(this).css('background-color', 'blue');
+      // Adds the player class
+      $(this).addClass('player2');
+      // Turns off click function after initial click
+      $(this).off("click");
+      // adds the current boxes id to relevent array
+      player2Array.push('#' + this.id);
+      // runs function to check win
+      checkWin(player2Array, "player 2");
+          console.log(player2Array);
+    }
   });
-// once a box has been clicked and changed colour
-// next clicked box changes to a different colour
 
-//a function, array or something logs these colour changes
-//and stores them
 
-//once three of these are logged in a 3 then game returns winner
-// if it is a tie without 3 in a row game calls tie
+
+  // if it is a tie without 3 in a row game calls tie
 
 });
